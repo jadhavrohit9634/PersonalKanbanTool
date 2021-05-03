126c6101dd0c2e799f6901d47cb7172730496465
Rohit Jadhav <jadhavrohit9634@gmail.com>
2021-05-03 13:43:31 +0530

    Added Custom Exception Handler for DB exception

--- PPMToolBackend/src/main/java/com/rjtech/ppmtool/services/ProjectService.java
+++ PPMToolBackend/src/main/java/com/rjtech/ppmtool/services/ProjectService.java
@@ -1,9 +1,12 @@
 package com.rjtech.ppmtool.services;
 
 import com.rjtech.ppmtool.domain.Project;
+import com.rjtech.ppmtool.exceptions.ProjectIdException;
 import com.rjtech.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 @Service
 public class ProjectService {
@@ -13,7 +16,12 @@ public class ProjectService {
 
     public Project saveOrUpdateProject(Project project) {
         //Business Logic will come here
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e){
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exist");
+        }
 
-       return projectRepository.save(project);
     }
 }
