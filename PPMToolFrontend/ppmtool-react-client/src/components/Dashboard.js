ee64b24b8d52d11cd8ee856c800f1689487a07aa
Rohit Jadhav <jadhavrohit9634@gmail.com>
2021-05-04 15:03:17 +0530

    Created Dashboard, Header and Project Component Setup

--- PPMToolFrontend/ppmtool-react-client/src/components/Dashboard.js
+++ PPMToolFrontend/ppmtool-react-client/src/components/Dashboard.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import ProjectItem from './Project/ProjectItem';
 
 class Dashboard extends Component {
     render() {
-        return <h1>Welcome to the Dashboard.</h1>;
-        
+        return (
+            <main>
+                <header>
+                    <h1>Welcome to the Dashboard.</h1>
+                </header>           
+                <ProjectItem />
+            </main>
+            
+        );
     }
 }
 
