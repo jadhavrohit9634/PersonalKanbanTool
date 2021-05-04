68c467a7dc530d29b60d5311761652dac81411d5
Rohit Jadhav <jadhavrohit9634@gmail.com>
2021-05-04 15:58:39 +0530

    Added Bootstrap and pre written styling to components

--- PPMToolFrontend/ppmtool-react-client/src/App.js
+++ PPMToolFrontend/ppmtool-react-client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import './components/Dashboard'
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
--- PPMToolFrontend/ppmtool-react-client/src/components/Dashboard.js
+++ PPMToolFrontend/ppmtool-react-client/src/components/Dashboard.js
@@ -4,12 +4,24 @@ import ProjectItem from './Project/ProjectItem';
 class Dashboard extends Component {
     render() {
         return (
-            <main>
-                <header>
-                    <h1>Welcome to the Dashboard.</h1>
-                </header>           
-                <ProjectItem />
-            </main>
+             <div className="projects">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="display-4 text-center">Projects</h1>
+                            <br />
+                            <a href="www.google.com" className="btn btn-lg btn-info">
+                                Create a Project
+                            </a>
+                            <br />
+                            <hr />
+
+                           <ProjectItem />
+                           <ProjectItem />
+                        </div>
+                    </div>
+                </div>
+    </div>
             
         );
     }
