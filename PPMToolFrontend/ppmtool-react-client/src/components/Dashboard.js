8f82a5b433a753b39a90f2448dbc6a6990a03bcf
Rohit Jadhav <jadhavrohit9634@gmail.com>
2021-05-10 14:04:40 +0530

    Added Get All Projects to DashBoard UI From Backend

--- PPMToolFrontend/ppmtool-react-client/src/components/Dashboard.js
+++ PPMToolFrontend/ppmtool-react-client/src/components/Dashboard.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
 import CreateProjectButton from './Project/CreateProjectButton';
 import ProjectItem from './Project/ProjectItem';
+import {connect} from 'react-redux';
+import { getProjects } from '../actions/projectActions'
+import PropTypes from "prop-types"
 
 class Dashboard extends Component {
+    
+    componentDidMount(){
+        this.props.getProjects();
+    }
+    
+    
     render() {
+        
+        const {projects} = this.props.project;
+        
         return (
              <div className="projects">
                 <div className="container">
@@ -14,9 +26,12 @@ class Dashboard extends Component {
                                 <CreateProjectButton />
                             <br />
                             <hr />
-
-                           <ProjectItem />
-                           <ProjectItem />
+                            {
+                                projects.map(project => {
+                                    return <ProjectItem key={project.id} project={project}/>
+                                })
+                            }
+                          
                         </div>
                     </div>
                 </div>
@@ -26,4 +41,13 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard;
+Dashboard.propTypes = {
+    project: PropTypes.object.isRequired,
+    getProjects: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+    project: state.project
+})
+
+export default connect( mapStateToProps, {getProjects} ) (Dashboard);
