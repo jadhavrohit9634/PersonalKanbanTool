ee58a4f4e2a8583153330ace56379bef0df2563a
Rohit Jadhav <jadhavrohit9634@gmail.com>
2021-05-11 13:29:15 +0530

    Added error handling to update from and bug fix

--- PPMToolFrontend/ppmtool-react-client/src/components/Project/UpdateProject.js
+++ PPMToolFrontend/ppmtool-react-client/src/components/Project/UpdateProject.js
@@ -15,7 +15,8 @@ class UpdateProject extends Component {
             projectIdentifier: "",
             description: "",
             start_date: "",
-            end_date: ""
+            end_date: "",
+            errors: {}
         }
         
         this.onChange = this.onChange.bind(this);
@@ -23,6 +24,11 @@ class UpdateProject extends Component {
     }
     
     componentWillReceiveProps(nextProps){
+        
+        if(nextProps.errors){
+            this.setState({errors:nextProps.errors})
+        }
+        
        const {id, projectName, projectIdentifier, description, start_date, end_date} = nextProps.project;
        
        this.setState({
@@ -59,6 +65,8 @@ class UpdateProject extends Component {
     }
     
     render() {
+        const {errors} = this.state;
+        
         return (
                <div className="project">
                     <div className="container">
@@ -70,12 +78,21 @@ class UpdateProject extends Component {
                                     <div className="form-group">
                                         <input 
                                             type="text" 
-                                            className="form-control form-control-lg " 
+                                            className={classnames("form-control form-control-lg",{
+                                                "is-invalid": errors.projectName
+                                            })}
                                             placeholder="Project Name"
                                             name="projectName"
                                             value={this.state.projectName}
                                             onChange={this.onChange} 
                                         />
+                                        {
+                                            errors.projectName && (
+                                            <div className="invalid-feedback">
+                                                {errors.projectName}
+                                            </div>
+                                            )
+                                        }
                                     </div>
                                     <div className="form-group">
                                         <input 
@@ -90,12 +107,19 @@ class UpdateProject extends Component {
                                     
                                     <div className="form-group">
                                         <textarea 
-                                            className="form-control form-control-lg" 
+                                            className={classnames("form-control form-control-lg",{
+                                                "is-invalid": errors.description
+                                            })} 
                                             placeholder="Project Description"
                                             name="description"
                                              value={this.state.description}
                                              onChange={this.onChange} 
                                         ></textarea>
+                                        {errors.description && (
+                                            <div className="invalid-feedback">
+                                                {errors.description}
+                                            </div>
+                                        )}
                                     </div>
                                     <h6>Start Date</h6>
                                     <div className="form-group">
@@ -129,11 +153,13 @@ class UpdateProject extends Component {
 UpdateProject.propTypes = {
     getProject: PropTypes.func.isRequired,
     createProject: PropTypes.func.isRequired,
-    project: PropTypes.object.isRequired
+    project: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-    project:state.project.project
+    project:state.project.project,
+    errors: state.errors
 })
 
 export default connect(mapStateToProps, {getProject, createProject}) (UpdateProject);
