ce8697df7ee06960e84e04848d903c70cc131601
Rohit Jadhav <jadhavrohit9634@gmail.com>
2021-05-30 12:58:50 +0530

    Added User Registration and Validation API

--- PPMToolBackend/src/main/java/com/rjtech/ppmtool/services/UserService.java
+++ PPMToolBackend/src/main/java/com/rjtech/ppmtool/services/UserService.java
@@ -1,6 +1,7 @@
 package com.rjtech.ppmtool.services;
 
 import com.rjtech.ppmtool.domain.User;
+import com.rjtech.ppmtool.exceptions.UsernameAlreadyExistsException;
 import com.rjtech.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,12 +17,18 @@ public class UserService {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public User saveUser(User newUser){
-       newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
 
-       //Username has to be unique (exception)
+        try {
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+            //Username has to be unique (exception)
+            newUser.setUsername(newUser.getUsername());
+            // Make sure password and confirm password match
+            // We dont persist or show the confirmPassword
+            newUser.setConfirmPassword("");
+            return userRepository.save(newUser);
+        } catch (Exception e){
+            throw new UsernameAlreadyExistsException("Username '" + newUser.getUsername() + "' already exists.");
+        }
 
-        // Make sure password and confirm password match
-        // We dont persist or show the confirmPassword
-       return userRepository.save(newUser);
     }
 }
